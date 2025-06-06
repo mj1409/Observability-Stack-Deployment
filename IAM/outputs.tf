4574876229478551a6981f72e054410163cbaf4b
mj1409 <mooregirishreddy@gmail.com>
2025-06-07 02:44:17 +0530

    modifying the correct resource name

--- IAM/outputs.tf
+++ IAM/outputs.tf
@@ -3,11 +3,11 @@ output "iam_user_name" {
 }
 
 output "access_key_id" {
-  value     = aws_iam_access_key.observability_user.id
+  value     = aws_iam_access_key.access_key.id
   sensitive = true
 }
 
 output "secret_access_key" {
-  value     = aws_iam_access_key.observability_user.secret
+  value     = aws_iam_access_key.access_key.secret
   sensitive = true
 }
