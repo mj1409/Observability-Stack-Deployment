d6e25959dc891fdad3f6c391cd63e100a3a171d7
mj1409 <mooregirishreddy@gmail.com>
2025-06-19 00:30:50 +0530

    enable backend config

--- terraform-backend/backend.tf
+++ terraform-backend/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-example"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
